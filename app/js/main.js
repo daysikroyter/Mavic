b7a3eb8aecd62ba042e8da25781f26e6caa0e49b
daysikroyter <nubchenko@gmail.com>
2024-06-06 00:28:59 +0300

    added header, section top, about & benefits

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(function() {
+  $('.about__list').slick({
+    prevArrow: '<button class="slick-arrow slick-prev" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="18" viewBox="0 0 10 18" fill="none"><path d="M9.21839 1L1 9L9.21839 17" stroke="white" /></svg></button>',
+    nextArrow: '<button class="slick-arrow slick-next" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="18" viewBox="0 0 10 18" fill="none"><path d="M9.21839 1L1 9L9.21839 17" stroke="white" /></svg></button>',
+    autoplay: true
+  });
+});
